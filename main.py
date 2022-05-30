3e8ea7c558978525d79105194dfe041a858b5926
Bartosz Augustyniak <bartagst@gmail.com>
2022-05-30 17:26:41 +0200

    added encode endpoint

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import flask
+from wtforms import StringField
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
 app = flask.Flask(__name__)
 
+def encode(text):
+    weird_text = text
+    return weird_text
 
 @app.get("/")
-def hello():
-    """Return a friendly HTTP greeting."""
-    return "Hello World!\n"
+def home():
+    # text = StringField('text')
+    return flask.render_template('home_layout.html')
 
+
+@app.route("/encode", methods=['POST'])
+def encode_endpoint():
+    text = flask.request.form['text']
+    return encode(text)
 
 
 if __name__ == "__main__":
