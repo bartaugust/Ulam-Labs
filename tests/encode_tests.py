f307553e37681e19235b7f5b71ed2bd2ebe570a2
Bartosz Augustyniak <bartagst@gmail.com>
2022-05-31 22:13:59 +0200

    added test_words_in_sentence

--- tests/encode_tests.py
+++ tests/encode_tests.py
@@ -6,7 +6,7 @@ four_letter_words = [
     ('Word', 'Wrod'),
     ('test', 'tset'),
     ('moje', 'mjoe'),
-    ('4567','4657'),
+    ('4567', '4657'),
 ]
 
 sentences = [
@@ -27,17 +27,21 @@ short_words = [
     ('123'),
 ]
 
+
 @pytest.mark.parametrize('word, expected_word', four_letter_words)
 def test_four_letter_word(word, expected_word):
     assert main.encode(word) == main.separator + expected_word + main.separator + word
+
 
 @pytest.mark.parametrize('word', short_words)
 def test_short_words(word):
     assert main.encode(word) == main.separator + word + main.separator
 
+
 @pytest.mark.parametrize('word', no_words)
 def test_no_words(word):
     assert main.encode(word) == main.separator + word + main.separator
+
 
 @pytest.mark.parametrize('sentence', sentences)
 def test_letters_in_words(sentence):
@@ -50,3 +54,16 @@ def test_letters_in_words(sentence):
     for i in range(len(original_words)):
         assert sorted(original_words[i]) == sorted(encoded_words[i])
 
+
+@pytest.mark.parametrize('sentence', sentences)
+def test_words_in_sentence(sentence):
+    tokenize_re = re.compile(r'(\w+)', re.U)
+    original_words = tokenize_re.findall(sentence)
+    test_words = []
+    encoded_sentence = main.encode(sentence)
+    _, _, words = encoded_sentence.split(main.separator)
+    for word in original_words:
+        if len(word) > 3:
+            if word[1:-1] != word[1] * len(word[1:-1]):
+                test_words.append(word)
+    assert ' '.join(sorted(test_words, key=str.lower)) == words
